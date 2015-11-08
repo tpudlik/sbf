775f7cd105ff2011802881ebebbec859fe9407e5
Ted Pudlik <tpudlik@gmail.com>
2015-11-08 11:30:13 -0500

    Improved dtypes of saved arrays in reference_points.

--- reference/reference_points.py
+++ reference/reference_points.py
@@ -1,3 +1,21 @@
+"""Generate (z, n) values.
+
+Real arguments are reated separately to prevent small imaginary parts in the
+input from leading to difficult-to-quash small imaginary parts of the output.
+
+The output is saved as two one-dimensional numpy arrays:
+
+    reference_points_real : [('z', 'f8'), ('n', 'u8')]
+        Contains RADIAL_POINTS * (MAX_ORDER + 1) elements, with the 
+        MAX_ORDER + 1 values of n iterated over first.
+    reference_points_complex : [('z', 'c16'), ('n', 'u8')]
+        Contains RADIAL_POINTS * (MAX_ORDER + 1) * ANGULAR_POINTS elements,
+        with the MAX_ORDER + 1 values of n iterated over first, then the
+        RADIAL_POINTS values of |z|, and finally the ANGULAR_POINTS values
+        of arg z.
+
+"""
+
 import itertools, sys
 from os import path
 import cPickle as pickle
@@ -8,7 +26,7 @@ import numpy as np
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
 from reference.config import (INNER_RADIUS, OUTER_RADIUS, RADIAL_POINTS,
-                                 ANGULAR_POINTS, MAX_ORDER)
+                              ANGULAR_POINTS, MAX_ORDER)
 
 def reference_points():
     """Returns an iterable of (point, order) pairs.
@@ -18,25 +36,36 @@ def reference_points():
 
     Returns
     -------
-    iterator : Yields tuples of (z, order)
+    iterator : Yields tuples of (z, order).  Real values occur before any
+        imaginary ones.  The total number of values returned is
+        RADIAL_POINTS * (MAX_ORDER + 1) * (ANGULAR_POINTS + 1).
 
     """
-    radii = 10**np.linspace(INNER_RADIUS, OUTER_RADIUS, RADIAL_POINTS,
-                            dtype=np.float64)
-    phases = np.exp(1j*np.arange(1, ANGULAR_POINTS + 1, dtype=np.float64)
+    return itertools.chain(reference_points_real(),reference_points_complex())
+
+
+def reference_points_real():
+    return itertools.product(iter(radius_array()), xrange(MAX_ORDER + 1))
+
+
+def reference_points_complex():
+    radii = radius_array()
+    phases = np.exp(1j*np.arange(1, ANGULAR_POINTS + 1, dtype=np.complex128)
                     *2*np.pi/(ANGULAR_POINTS + 1))
-    
-    real_points = iter(radii) # Treated separately to prevent small imaginary
-                              # parts in the input from leading to difficult-
-                              # to-quash small imaginary parts of the output.
     complex_points = iter(np.outer(radii, phases).flatten())
     orders = xrange(MAX_ORDER + 1)
-    return itertools.chain(itertools.product(real_points, orders),
-                           itertools.product(complex_points, orders))
+    return itertools.product(complex_points, orders)
+
+
+def radius_array():
+    return 10**np.linspace(INNER_RADIUS, OUTER_RADIUS, RADIAL_POINTS,
+                           dtype=np.float64)
 
 
 if __name__ == "__main__":
-    with open("reference_points.pickle", "wb") as f:
-        pickle.dump(reference_points(), f)
-    np.save("reference_points.npy", np.array(list(reference_points()),
-                                             dtype=np.complex128))
+    np.save("reference_points_real.npy",
+            np.array(list(reference_points_real()),
+                     dtype=[('z', 'f8'), ('n', 'u8')]))
+    np.save("reference_points_complex.npy",
+            np.array(list(reference_points_complex()),
+                     dtype=[('z', 'c16'), ('n', 'u8')]))
